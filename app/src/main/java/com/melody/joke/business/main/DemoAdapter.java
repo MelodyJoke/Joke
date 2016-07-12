a13c07c600ca8cd6f00819bf2463a60f0141599c
MelodyJoke <melochanladyjj@gmail.com>
2016-07-12 15:57:43 +0800

    a demo of a tabbed activity

--- app/src/main/java/com/melody/joke/business/main/DemoAdapter.java
+++ app/src/main/java/com/melody/joke/business/main/DemoAdapter.java
@@ -3,7 +3,6 @@ package com.melody.joke.business.main;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
